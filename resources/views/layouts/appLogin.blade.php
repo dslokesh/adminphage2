a635afbd641ae7563f6f728e4e82b5636e793903
Lokesh <sharmalk1991@gmail.com>
2024-02-18 16:38:50 +0530

    sdsd

--- resources/views/layouts/appLogin.blade.php
+++ resources/views/layouts/appLogin.blade.php
@@ -343,6 +343,7 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script>
  $(document).ready(function () {
+	  $("#loader-overlay").hide();
     $('.datepickerAgent').datepicker({
         weekStart: 1,
         minDate: 0,
