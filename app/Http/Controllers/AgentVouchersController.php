86d16ae47555e73191b127b7151be12d205e8d0c
Lokesh <sharmalk1991@gmail.com>
2024-03-02 15:33:55 +0530

    ddsd

--- app/Http/Controllers/AgentVouchersController.php
+++ app/Http/Controllers/AgentVouchersController.php
@@ -435,7 +435,7 @@ class AgentVouchersController extends Controller
        $data = $request->all();
 		$typeActivities = config("constants.typeActivities"); 
         //$perPage = config("constants.ADMIN_PAGE_LIMIT");
-		$perPage = "1";
+		$perPage = "5";
 		$voucher = Voucher::find($vid);
 		$startDate = $voucher->travel_from_date;
 		$endDate = $voucher->travel_to_date;
@@ -513,7 +513,7 @@ class AgentVouchersController extends Controller
 
 		$data = $request->all();
 		$typeActivities = config("constants.typeActivities"); 
-		$perPage = "1";
+		$perPage = "5";
 
 		// Find the voucher based on $vid
 		$voucher = Voucher::find($vid);
